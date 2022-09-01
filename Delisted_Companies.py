956827cca9eb2da5a32d9f45d9706825378e3ecf
pleum-attakorn <plumjai11070@hotmail.com>
2022-09-01 22:49:09 +0700

    get data and save to db

--- Delisted_Companies.py
+++ Delisted_Companies.py
@@ -6,19 +6,9 @@ import database
 api_key = pd.read_csv('./key.txt', header=None)[0][0]
 URL = 'https://financialmodelingprep.com/api/v3/'
 data = 'delisted-companies'
-page = 0
+#page = 0
 
 con_str = database.GetConnectionString()
-
-def select():
-    sql = '''
-        SELECT * FROM Delisted_Companies
-    '''
-    with pyodbc.connect(con_str) as con:
-        cursor = con.cursor()
-        for row in cursor.execute(sql):
-            print(row)
-        cursor.close()
 
 def create_table():
     sql = '''
@@ -59,10 +49,8 @@ def insert_data(data):
     '''
     with pyodbc.connect(con_str) as con:
         cursor = con.cursor()
-        cursor.execute(sql, data)
+        cursor.executemany(sql, data)
         cursor.close()
-
-
 
 if __name__ == '__main__':    
 
@@ -70,10 +58,13 @@ if __name__ == '__main__':
         clear_data()
     else:
         create_table()
-
-    r = requests.get('{}{}?page={}&apikey={}'.format(URL, data, page, api_key))
-
-    df = pd.DataFrame.from_dict(r.json())
     
-    #print(list(df.iloc[0]))
+    df = pd.DataFrame()
+    for i in range(0, 57):
+        page = i
+        r = requests.get('{}{}?page={}&apikey={}'.format(URL, data, page, api_key))
+        df2 = pd.DataFrame.from_dict(r.json())
+        df = pd.concat([df, df2])
     
+    df.reset_index(drop=True, inplace=True)
+    insert_data(df.values.tolist())
