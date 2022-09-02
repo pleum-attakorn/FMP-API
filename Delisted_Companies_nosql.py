86335cc4a0f93125932d6e927da7f3b3b940a742
pleum-attakorn <plumjai11070@hotmail.com>
2022-09-02 19:36:43 +0700

    get and save multiple data

--- Delisted_Companies_nosql.py
+++ Delisted_Companies_nosql.py
@@ -13,29 +13,26 @@ db = myclient['FMP-API']
 mycoll = db['Delisted_Companies']
 mycoll.delete_many({})
 
-# data = mycoll.find({})
-# print(list(data))
-
 api_key = pd.read_csv('../key.txt', header=None)[0][0]
 URL = 'https://financialmodelingprep.com/api/v3/'
 data = 'delisted-companies'
-page = 0
 
-r = requests.get('{}{}?page={}&apikey={}'.format(URL, data, page, api_key))
-df2 = pd.DataFrame.from_dict(r.json())
-for i in range(0, len(df2)):
-    lst = list(df2.iloc[i])
-    mycoll.insert_one({'_id': i, 'symbol' : lst[0],  'companyName' : lst[1], 'exchange' : lst[2], 'ipoDate': lst[3], 'delistedDate': lst[4]})
-
-# df = pd.DataFrame()
-# for i in range(0, 57):
-#     page = i
-#     r = requests.get('{}{}?page={}&apikey={}'.format(URL, data, page, api_key))
-#     df2 = pd.DataFrame.from_dict(r.json())
-#     df = pd.concat([df, df2])
-# df.reset_index(drop=True, inplace=True)
-# for i in range(0, len(df)):
-#     lst = list(df.iloc[i])
+# page = 0
+# r = requests.get('{}{}?page={}&apikey={}'.format(URL, data, page, api_key))
+# df2 = pd.DataFrame.from_dict(r.json())
+# for i in range(0, len(df2)):
+#     lst = list(df2.iloc[i])
 #     mycoll.insert_one({'_id': i, 'symbol' : lst[0],  'companyName' : lst[1], 'exchange' : lst[2], 'ipoDate': lst[3], 'delistedDate': lst[4]})
+
+df = pd.DataFrame()
+for i in range(0, 57):
+    page = i
+    r = requests.get('{}{}?page={}&apikey={}'.format(URL, data, page, api_key))
+    df2 = pd.DataFrame.from_dict(r.json())
+    df = pd.concat([df, df2])
+df.reset_index(drop=True, inplace=True)
+for i in range(0, len(df)):
+    lst = list(df.iloc[i])
+    mycoll.insert_one({'_id': i, 'symbol' : lst[0],  'companyName' : lst[1], 'exchange' : lst[2], 'ipoDate': lst[3], 'delistedDate': lst[4]})
     
 
